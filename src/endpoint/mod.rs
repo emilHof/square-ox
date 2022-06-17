da73aa882170a5c92611e0a3496b411747b9ec16
emilHof <95590295+emilHof@users.noreply.github.com>
2022-06-17 08:40:39 -0400

    added new endpoints and an EndpointVerb enum

--- src/endpoint/mod.rs
+++ src/endpoint/mod.rs
@@ -6,6 +6,10 @@ the Display trait for the URL of all of the endpoints
  */
 
 pub mod payment;
+pub mod bookings;
+pub mod locations;
+pub mod catalog;
+pub mod customers;
 
 use crate::client::ClientMode;
 use crate::client::SquareClient;
@@ -16,7 +20,21 @@ use std::fmt;
 #[non_exhaustive]
 pub enum SquareEndpoint {
     Payments,
-    Bookings
+    Bookings,
+    BookingsAvailabilitySearch,
+    Locations,
+    Catalog,
+    Customers
+}
+
+/// All of the HTTP verbs that have been implemented and are accepted by the different
+/// [Square API](https://developer.squareup.com) endpoints.
+pub enum EndpointVerb {
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
 }
 
 /// Implement the Display trait for all of the endpoints we need this allows
@@ -26,7 +44,11 @@ impl fmt::Display for SquareEndpoint {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             SquareEndpoint::Payments => write!(f, "payments"),
-            SquareEndpoint::Bookings => write!(f, "bookings")
+            SquareEndpoint::Bookings => write!(f, "bookings"),  // TODO Implement Bookings
+            SquareEndpoint::BookingsAvailabilitySearch => write!(f, "bookings/availability/search"),  // TODO Implement Bookings
+            SquareEndpoint::Locations => write!(f, "locations"),  // TODO Implement Locations
+            SquareEndpoint::Catalog => write!(f, "catalog"),  // TODO Implement Catalog
+            SquareEndpoint::Customers => write!(f, "customers"),  // TODO Implement Customers
         }
     }
 }
