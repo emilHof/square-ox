bbd5f7e3489318f3fa8320e2e07b551c5bdb3fc3
emilHof <95590295+emilHof@users.noreply.github.com>
2022-06-19 21:05:25 -0400

    adjusted locations.rs to adhere to the new client.request() contract

--- src/endpoint/locations.rs
+++ src/endpoint/locations.rs
@@ -10,7 +10,12 @@ use serde::{Deserialize, Serialize};
 // TODO implement the SquareClient
 impl SquareClient {
     pub async fn list_locations(&self) -> Result<SquareResponse, SquareError> {
-        self.request::<Locations>(EndpointVerb::GET ,SquareEndpoint::Locations, None).await
+        self.request(
+            EndpointVerb::GET,
+            SquareEndpoint::Locations,
+            None::<&Location>,
+            None,
+        ).await
     }
 }
 
@@ -40,7 +45,6 @@ impl LocationsBuilder {
 #[cfg(test)]
 mod test_locations {
     use super::*;
-    use dotenv::dotenv;
 
     #[actix_rt::test]
     async fn test_locations_builder_success() {
@@ -51,8 +55,13 @@ mod test_locations {
 
     #[actix_rt::test]
     async fn test_create_location_request() {
-        let sut = SquareClient::new("EAAAEIQAdp294a9PHkqjSlWhi3qWBHnUwposv1n-TZrn0XW6xHGg810-72UrNr6U");
-        let actual = sut.list_locations().await;
-        assert!(actual.is_ok())
+        use dotenv::dotenv;
+        use std::env;
+
+        dotenv().ok();
+        let access_token = env::var("ACCESS_TOKEN").expect("ACCESS_TOKEN to be set");
+        let sut = SquareClient::new(&access_token);
+        let result = sut.list_locations().await;
+        assert!(result.is_ok())
     }
 }
