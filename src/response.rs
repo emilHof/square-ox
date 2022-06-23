43ad9d1d00cf6ae2d8d95ff18b421250c9c5adfb
emilHof <95590295+emilHof@users.noreply.github.com>
2022-06-22 21:31:06 -0400

    added some more documentation

--- src/response.rs
+++ src/response.rs
@@ -1,10 +1,22 @@
+/*!
+This defines the possible responses you could receive from the
+[Square API](https://developer.squareup.com).
+
+The most of the structs have almost all of their fields set as optional as this makes dealing
+with the [Square API](https://developer.squareup.com)'s response pattern more manageable.
+ */
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[non_exhaustive]
+/// The [SquareResponse](SquareResponse) response defines the generic response type that encompasses
+/// almost all possible [Square API](https://developer.squareup.com) responses. All fields are
+/// optional to allow for handling of possible errors returned by the
+/// [Square API](https://developer.squareup.com).
 pub struct SquareResponse {
     #[serde(flatten)]
-    pub response: crate::response::jsons::Response,
+    pub response: Option<crate::response::jsons::Response>,
     #[serde(default)]
     pub errors: Option<Vec<ResponseError>>,
     #[serde(default)]
@@ -13,6 +25,10 @@ pub struct SquareResponse {
     pub id_mapping: Option<Vec<(String, String)>>,
 }
 
+
+/// The [ResponseError](ResponseError) defines the error schema returned by the
+/// [Square API](https://developer.squareup.com) should an error occur. This makes error handling
+/// possible by checking if the error field of the [SquareResponse](SquareResponse) is some.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ResponseError {
     pub category: String,
@@ -24,6 +40,12 @@ pub struct ResponseError {
 }
 
 pub mod enums {
+    /*!
+    The [enums](enums) module defines the Enums expected and returned by the
+    [Square API](https://developer.squareup.com). They are used for verifying input for multiple
+    builder functions.
+     */
+
     use std::collections::HashSet;
 
     pub struct CatalogObjectTypeEnum(HashSet<String>);
@@ -361,11 +383,11 @@ pub mod jsons {
     #[derive(Clone, Serialize, Debug, Deserialize)]
     pub struct FilterValue {
         #[serde(skip_serializing_if = "Option::is_none")]
-        all: Option<Vec<String>>,
+        pub  all: Option<Vec<String>>,
         #[serde(skip_serializing_if = "Option::is_none")]
-        any: Option<Vec<String>>,
+        pub any: Option<Vec<String>>,
         #[serde(skip_serializing_if = "Option::is_none")]
-        none: Option<Vec<String>>,
+        pub none: Option<Vec<String>>,
     }
 
     #[derive(Clone, Serialize, Debug, Deserialize)]
